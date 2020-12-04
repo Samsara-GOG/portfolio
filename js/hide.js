d644313065f93093f5251b9ef2848892c9eaa57c
Samsara <samsaraonaya@gmail.com>
2020-12-04 11:11:19 +0100

    Ajout du script javascript pour le retour en haut de page
    Retour qui disparait et reapparait en fonction de l'avancement dans le scrolling

---
+++ js/hide.js
@@ -0,0 +1,27 @@
+
+
+ScrollToTop = function() {
+    let s = $(window).scrollTop();
+    if (s > 250) {
+      $('#arrow-up').fadeIn();
+    } else {
+      $('arrow-up').fadeOut();
+    }
+   
+    $('arrow-up').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+  }
+   
+  StopAnimation = function() {
+    $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
+      $('html, body').stop();
+    });
+  }
+   
+   
+  $(window).scroll(function() {
+    ScrollToTop();
+    StopAnimation();
+  });
